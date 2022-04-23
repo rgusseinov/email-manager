6ccec4865cb72bb5af8816fb864e2ae588a4fe31
= <r.@mail.ru>
2022-04-23 16:56:14 +0600

    feat: add sent app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import Header from './components/header/header';
 import MailForm from './components/mail-sender/mail-form';
 import '../src/scss/app.scss';
 import MessageSent from './components/message-status/message-sent';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { clearQueue } from './store/emailSlice';
 
 function App() {
-  const { isEmailInQueue } = useSelector((data) => data.emails);
+  const { isEmailInQueue, emails } = useSelector((data) => data.emails);
   const handleDragStart = (e) => e.preventDefault();
-  console.log(`isEmailInQueue`, isEmailInQueue);
+  const timerRef = useRef();
+  const dispatch = useDispatch();
+  console.log(`emails`, emails);
+
+  useEffect(() => {
+    setInterval(() => {
+      dispatch(clearQueue());
+    }, 2000);
+    return () => clearInterval(timerRef.current);
+  }, []);
+
   return (
     <div className="app" onDragStart={handleDragStart}>
       <div className="content">
         <Header />
-        <MailForm />
-        <br />
-        <MessageSent />
+        {isEmailInQueue ? <MessageSent /> : <MailForm />}
       </div>
     </div>
   );
--- src/store/emailSlice.js
+++ src/store/emailSlice.js
@@ -10,6 +10,7 @@ const emailSlice = createSlice({
   reducers: {
     addEmail(state, action) {
       // console.log(`payload:`, action.payload.subject);
+      state.isEmailInQueue = true;
       state.emails.push({
         id: new Date().toDateString(),
         subject: action.payload.subject,
@@ -19,10 +20,13 @@ const emailSlice = createSlice({
         sendToEmail: action.payload.sendToEmail,
         message: action.payload.message
       });
+    },
+    clearQueue(state) {
+      if (state.isEmailInQueue) state.isEmailInQueue = false;
     }
   }
 });
 
-export const { addEmail } = emailSlice.actions;
+export const { addEmail, clearQueue } = emailSlice.actions;
 
 export default emailSlice.reducer;
